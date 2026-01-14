fe07d2ab2b0f25a655e81d0a875996b4845303a7
Leclowndu93150 <xdr.a123454321@gmail.com>
2026-01-14 12:34:58 +0100

    fix permissions

--- src/main/java/com/leclowndu93150/hyssentials/commands/teleport/TpCommand.java
+++ src/main/java/com/leclowndu93150/hyssentials/commands/teleport/TpCommand.java
@@ -13,6 +13,7 @@ import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayer
 import com.hypixel.hytale.server.core.modules.entity.component.HeadRotation;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.modules.entity.teleport.Teleport;
+import com.hypixel.hytale.server.core.permissions.HytalePermissions;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.Universe;
 import com.hypixel.hytale.server.core.universe.world.World;
@@ -28,6 +29,7 @@ public class TpCommand extends AbstractPlayerCommand {
     public TpCommand(@Nonnull BackManager backManager) {
         super("tp", "Teleport to a player (admin)");
         this.backManager = backManager;
+        this.requirePermission(HytalePermissions.fromCommand("hyssentials.tp"));
     }
 
     @Override
--- src/main/java/com/leclowndu93150/hyssentials/commands/teleport/TphereCommand.java
+++ src/main/java/com/leclowndu93150/hyssentials/commands/teleport/TphereCommand.java
@@ -13,6 +13,7 @@ import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayer
 import com.hypixel.hytale.server.core.modules.entity.component.HeadRotation;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.modules.entity.teleport.Teleport;
+import com.hypixel.hytale.server.core.permissions.HytalePermissions;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.Universe;
 import com.hypixel.hytale.server.core.universe.world.World;
@@ -28,6 +29,7 @@ public class TphereCommand extends AbstractPlayerCommand {
     public TphereCommand(@Nonnull BackManager backManager) {
         super("tphere", "Teleport a player to you (admin)");
         this.backManager = backManager;
+        this.requirePermission(HytalePermissions.fromCommand("hyssentials.tphere"));
     }
 
     @Override
